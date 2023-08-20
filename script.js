055801aa9fb785de84c0fa3d5f6b02a78d1b975a
bng16 <biswajitnag161619@gmail.com>
2023-08-21 04:28:32 +0530

    fetch the data from api and add it into the app.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const apilink="https: / /api.openweathermap.org/data/2.5/weather?units=metric&g=bangalore";
-const apikey="a8ed953e43365c4db0216ea87e9cfcf0";
+const apilink="https://api.openweathermap.org/data/2.5/weather?q="
 
-function checkWeather(){
-    const response= await fetch(apilink+`&appid=${apikey}`)
+const unitApikey="&units=metric&appid=a8ed953e43365c4db0216ea87e9cfcf0";
+
+const searchbox=document.querySelector("#search-box")
+const searchbutt=document.querySelector("#search-butt")
+
+
+async function checkWeather(city){
+    const response= await fetch(apilink+city+unitApikey);
 
     var data=await response.json();
 
-    console.log(data)
+    console.log(data);
+
+    document.querySelector("#city").innerHTML=data.name;
+    document.querySelector("#temp").innerHTML=Math.round(data.main.temp) +"°C";
+    document.querySelector("#Humidity").innerHTML=`${data.main.humidity}%`;
+    document.querySelector("#wind").innerHTML=`${data.wind.speed} km/hr`;
+
+
+
+
 }
 
-checkWeather()
+searchbutt.addEventListener("click", ()=>{
+    
+    checkWeather(searchbox.value.trim());
+
+})
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=Agartala&appid=a8ed953e43365c4db0216ea87e9cfcf0&units=metric
