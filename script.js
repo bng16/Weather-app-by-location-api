37cbd25b9ec58bf3a2f4fc52a3774121a7a3e3a6
bng16 <biswajitnag161619@gmail.com>
2023-08-21 06:28:53 +0530

    bg video added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const unitApikey="&units=metric&appid=a8ed953e43365c4db0216ea87e9cfcf0";
 const searchbox=document.querySelector("#search-box")
 const searchbutt=document.querySelector("#search-butt")
 
+const weatherIcon=document.querySelector("#weather-icon")
+const bgVideo=document.querySelector("#bg-video")
+
 
 async function checkWeather(city){
     const response= await fetch(apilink+city+unitApikey);
@@ -18,7 +21,36 @@ async function checkWeather(city){
     document.querySelector("#Humidity").innerHTML=`${data.main.humidity}%`;
     document.querySelector("#wind").innerHTML=`${data.wind.speed} km/hr`;
 
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="asset/clouds.png";
+        bgVideo.src="asset/clouds"
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="asset/clear.png";
+        bgVideo.src="asset/clear";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="asset/rain.png";
+        bgVideo.src="asset/rain.mp4";
 
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="asset/drizzle.png";
+        bgVideo.src="asset/drizzle.mp4";
+
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="asset/mist.png";
+        bgVideo.src="asset/mist.mp4";
+
+    }
+    else if(data.weather[0].main=="Snow"){
+        weatherIcon.src="asset/snow.png";
+        bgVideo.src="asset/snow.mp4";
+
+    }
+
+    document.querySelector("#section2").style.display="flex";
 
 
 }
